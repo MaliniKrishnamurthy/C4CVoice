8fd9025b6e92a280d7d24445c2cf4a8c3cf1c127
malini <malini.krishnamurthy@sap.com>
2018-04-11 21:17:29 +0530

    [Dummy]

---
+++ src/java/com/myVoice/Instruction.java
@@ -0,0 +1,34 @@
+package com.myVoice;
+
+//import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Instruction {
+    //Timestamp time;
+
+    ArrayList<ActionSchema> actionList;
+
+    Instruction() {
+    }
+
+    /*  @XmlElement
+	public Timestamp getTime() {
+		return time;
+	}
+
+	public void setTime(Timestamp time) {
+		this.time = time;
+	}*/
+    @XmlElement
+    public ArrayList<ActionSchema> getAction() {
+        return actionList;
+    }
+
+    public void setActionList(ArrayList<ActionSchema> actionList) {
+        this.actionList = actionList;
+    }
+}
